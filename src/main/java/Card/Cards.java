d52ebee5c8eeb0b92d355164a8ab8d43d488cab4
tahawanted <42439207+tahawanted@users.noreply.github.com>
2020-03-18 00:04:16 +0330

    trying

--- src/main/java/Card/Cards.java
+++ src/main/java/Card/Cards.java
@@ -4,14 +4,23 @@ interface playable{}
 interface vulnerable{}
 interface battlecry{}
 interface charge{}
+interface rush{}
 interface attack{}
-interface quest{}
 interface deathRattle{}
 interface taunt{}
 interface freeze{}
 interface discover{}
+interface reborn{}
+interface lifeSteal{}
+interface restore{}
+interface inspire{}
+interface divineShield{}
+interface windFury{}
+interface poisonous{}
+interface echo{}
+interface twinEcho{}
 interface stealth{}
-interface dead{}
+
 
 public class Cards{
     public static class card implements playable{
@@ -26,10 +35,13 @@ public class Cards{
             deathRattle, taunt, freeze, discover, stealth{
         String health;
         String attack;
+        boolean battlecry, charge, deathRattle, taunt, freeze, discover, stealth;
     }
     public static class spell extends card{}
     public static class quest extends card{}
-    public static class weapon extends card{}
+    public static class weapon extends card{
+        int durability;
+    }
     public static class heroPower extends spell{}
 
 }
